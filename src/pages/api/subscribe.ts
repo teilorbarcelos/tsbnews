6bc99a4c1a19515a0eb221e5ed059cbc7188b889
Teilor Souza Barcelos <tsb-@live.com>
2021-12-10 19:45:03 -0300

    fix: do not recreate stripe customer on subscribe

--- src/pages/api/subscribe.ts
+++ src/pages/api/subscribe.ts
@@ -1,18 +1,55 @@
-import { NextApiRequest, NextApiResponse } from "next";
+import { NextApiRequest, NextApiResponse } from "next"
 import { getSession } from 'next-auth/react'
-import { stripe } from "../../services/stripe";
+import { fauna } from "../../services/fauna"
+import { stripe } from "../../services/stripe"
+import { query as q } from 'faunadb'
+
+interface IUser {
+  ref: {
+    id: string
+  }
+  data: {
+    stripe_customer_id: string
+  }
+}
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   if (req.method === 'POST') {
     const session = getSession({ req })
 
-    const stripeCustomer = await stripe.customers.create({
-      email: (await session).user.email,
-      // metadata
-    })
+    const user = await fauna.query<IUser>(
+      q.Get(
+        q.Match(
+          q.Index('user_by_email'),
+          q.Casefold((await session).user.email)
+        )
+      )
+    )
+
+    let customerId = user.data.stripe_customer_id
+
+    if (!customerId) {
+      const stripeCustomer = await stripe.customers.create({
+        email: (await session).user.email,
+        // metadata
+      })
+
+      await fauna.query(
+        q.Update(
+          q.Ref(q.Collection('users'), user.ref.id),
+          {
+            data: {
+              stripe_customer_id: stripeCustomer.id
+            }
+          }
+        )
+      )
+
+      customerId = stripeCustomer.id
+    }
 
     const stripeCheckoutSession = await stripe.checkout.sessions.create({
-      customer: stripeCustomer.id,
+      customer: customerId,
       payment_method_types: ['card'],
       billing_address_collection: 'required',
       line_items: [
