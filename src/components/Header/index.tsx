4443230b02034958a3be1848d284a802190b1438
Teilor Souza Barcelos <tsb-@live.com>
2021-12-11 20:03:00 -0300

    prismic cms added and listing posts

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,13 +1,19 @@
+import Link from 'next/link'
 import SignInButton from '../SignInButton'
 import styles from './styles.module.scss'
+
 export function Header() {
   return (
     <header className={styles.container}>
       <div>
         <img src="/images/logo.svg" alt="tsb.news logo" />
         <nav>
-          <a className={styles.active}>Home</a>
-          <a>Posts</a>
+          <Link href="/">
+            <a className={styles.active}>Home</a>
+          </Link>
+          <Link href="/posts">
+            <a>Posts</a>
+          </Link>
         </nav>
         <SignInButton />
       </div>
